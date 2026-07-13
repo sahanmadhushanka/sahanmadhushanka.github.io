4de44d97b4fb8d9df4024bbdd7600d686442d16f
sahanmadhushanka <sahanmadhushanka679@gmail.com>
2026-07-13 20:48:30 +0000

    Deploying to gh-pages from @ sahanmadhushanka/sahanmadhushanka.github.io@6c899811c6ba76d20b2ae90edfaa05421e65e37c 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "815620066" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "1876277394" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
